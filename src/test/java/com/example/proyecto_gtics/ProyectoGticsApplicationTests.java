6419a6c5e8126d43358c6dea462a33581be38782
WalkedPlate <144744565+WalkedPlate@users.noreply.github.com>
2024-04-05 22:46:36 -0500

    Initial commit

---
+++ src/test/java/com/example/proyecto_gtics/ProyectoGticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoGticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
