b6cd551b1cb3c88b9f577cc6a897d13a23462195
eds0c <eduardo.carbajal@pucp.edu.pe>
2024-05-06 22:35:02 -0500

    En farmaciaWeb: linkeo de vistas FarmaciaWeb, vistas de las categorias y productos en sus respectivas categorías. Modificación en el carrito. Falta modificar el chatbot

--- src/main/java/com/example/proyecto_gtics/controller/FarmaciaWebVentaController.java
+++ src/main/java/com/example/proyecto_gtics/controller/FarmaciaWebVentaController.java
@@ -6,7 +6,9 @@ import com.example.proyecto_gtics.repository.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -51,7 +53,19 @@ public class FarmaciaWebVentaController {
     @GetMapping(value ={"/clinicarenacer"})
     public String paginaPrincipal(Model model){
 
+
+        List<Productos> listaProductos = productosRepository.findAll();
+        List<Productos> listarProducto = productosRepository.findAll();
         List<Categorias> listaCategorias = categoriasRepository.findAll();
+        List<Long> listaCantidades = new ArrayList<>();
+        for (Categorias categorias: listaCategorias){
+            listaCantidades.add(productosRepository.countByCategorias(categorias));
+        }
+
+
+        model.addAttribute("listaCantCategorias",listaCantidades);
+        model.addAttribute("listaProductos",listaProductos);
+        model.addAttribute("listarProducto",listarProducto);
         model.addAttribute("listaCategorias", listaCategorias);
         return "FarmaciaWebVenta/index";
     }
@@ -70,10 +84,22 @@ public class FarmaciaWebVentaController {
     }
 
     @GetMapping(value ={"/clinicarenacer/categoria"})
-    public String ordenarPorCategoria(Model model){
-        List<Productos> listaProductos = productosRepository.findAll();
+    public String ordenarPorCategoria(Model model,  @RequestParam(name = "idCategoria") int idCategoria){
+        Categorias categorias = categoriasRepository.findById(idCategoria).get();
+
+        List<Productos> listaProductosPorCategoria = productosRepository.findByCategorias(categorias);
+        List<Productos> listarProducto = productosRepository.findAll();
         List<Categorias> listaCategorias = categoriasRepository.findAll();
-        model.addAttribute("listaProductos",listaProductos);
+        List<Long> listaCantidades = new ArrayList<>();
+        for (Categorias categoria: listaCategorias){
+            listaCantidades.add(productosRepository.countByCategorias(categoria));
+        }
+        model.addAttribute("listaCantCategorias",listaCantidades);
+
+
+        model.addAttribute("categoriaActual",categorias);
+        model.addAttribute("listaProductos",listaProductosPorCategoria);
+        model.addAttribute("listarProducto",listarProducto);
         model.addAttribute("listaCategorias", listaCategorias);
         return "FarmaciaWebVenta/PorCategoria";
 
@@ -90,12 +116,32 @@ public class FarmaciaWebVentaController {
     }
 
     @GetMapping(value ={"/clinicarenacer/producto"})
-    public String producto(){
+    public String producto(Model model, @RequestParam(name = "idProductos") int idProductos){
+
+        Productos productos = productosRepository.findById(idProductos).get();
+        List<Productos> listaProductos = productosRepository.findAll();
+        List<Categorias> listaCategorias = categoriasRepository.findAll();
+        List<Long> listaCantidades = new ArrayList<>();
+        for (Categorias categoria: listaCategorias){
+            listaCantidades.add(productosRepository.countByCategorias(categoria));
+        }
+
+        model.addAttribute("productoActual",productos);
+
+        model.addAttribute("listaCantCategorias",listaCantidades);
+        model.addAttribute("listaProductos",listaProductos);
+        model.addAttribute("listaCategorias", listaCategorias);
         return "FarmaciaWebVenta/producto";
     }
 
     @GetMapping(value ={"/clinicarenacer/paciente/carrito"})
-    public String carrito(){
+    public String carrito(Model model){
+        Productos producto = new Productos();
+        List<Productos> productos = new ArrayList<>();
+        List<Categorias> listaCategorias = categoriasRepository.findAll();
+        model.addAttribute("listaCategorias", listaCategorias);
+        model.addAttribute("ListarProductos",productos);
+
         return "FarmaciaWebVenta/carrito";
     }
 
