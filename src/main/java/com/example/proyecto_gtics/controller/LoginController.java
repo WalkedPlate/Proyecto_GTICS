0e054e907f0ec23564ed0c6a9bc0849c48a3a1c5
Allison <a20196483@pucp.edu.pe>
2024-05-12 04:18:51 -0500

    mensajes de registro del login

--- src/main/java/com/example/proyecto_gtics/controller/LoginController.java
+++ src/main/java/com/example/proyecto_gtics/controller/LoginController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Optional;
@@ -68,12 +69,13 @@ public class LoginController {
     }
 
     @PostMapping(value = {"/guardarPaciente"})
-    public  String guardarPaciente(Usuarios paciente, @RequestParam("nombres") String nombres , @RequestParam("apellidos") String apellidos){
+    public  String guardarPaciente(Usuarios paciente, @RequestParam("nombres") String nombres , @RequestParam("apellidos") String apellidos, RedirectAttributes attr){
 
         //Comprobar si existe el paciente:
         Optional<Usuarios> consultaPaciente = usuariosRepository.findByDni(paciente.getDni());
 
         if(consultaPaciente.isPresent()){
+            attr.addFlashAttribute("err","Ya existe un usuario con los datos ingresados.");
             return "redirect:/login";
         }else {
             paciente.setNombre(nombres + ' ' + apellidos);
@@ -81,6 +83,7 @@ public class LoginController {
             paciente.setContrasena("Temporal_password");
             paciente.setTipoUsuario(tipoUsuarioRepository.findById("Paciente").get());
             usuariosRepository.save(paciente);
+            attr.addFlashAttribute("msg","Registro exitoso, se le enviarán sus credenciales por correo.");
         }
         return "redirect:/login";
     }
