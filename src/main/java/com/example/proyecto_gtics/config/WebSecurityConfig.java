d985e48252b6aa88e9590a8633d14f96d278125a
WalkedPlate <144744565+WalkedPlate@users.noreply.github.com>
2024-06-27 15:18:59 -0500

    enable csrf

--- src/main/java/com/example/proyecto_gtics/config/WebSecurityConfig.java
+++ src/main/java/com/example/proyecto_gtics/config/WebSecurityConfig.java
@@ -87,7 +87,7 @@ public class WebSecurityConfig {
                 .deleteCookies("JSESSIONID")
                 .invalidateHttpSession(true));
 
-        http.csrf(a -> a.disable());
+        //http.csrf(a -> a.disable());
 
         return http.build();
     }
